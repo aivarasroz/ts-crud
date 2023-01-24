caf687e4dabcad51d9ae1f5dc8939e23efc7ae2f
Aivaras Roz <aivaras.roz@gmail.com>
2023-01-24 19:52:27 +0200

    init: constructor and init methode created 4.3

--- užduotis/src/components/table.ts
+++ užduotis/src/components/table.ts
@@ -10,4 +10,19 @@ class Table<Type> {
   private props: TableProps<Type>;
   private tbody: HTMLTableSectionElement;
   private thead: HTMLTableSectionElement;
+
+  constructor(props: TableProps<Type>) {
+    this.props = props;
+    this.thead = document.createElement('thead');
+    this.tbody = document.createElement('tbody');
+    this.htmlElement = document.createElement('table');
+    this.initialize();
+  }
+
+
+  private initialize = (): void => {
+    this.htmlElement.className = 'table table-striped order border p-3';
+    this.htmlElement.append(
+      this.thead,
+      this.tbody);};
 }
