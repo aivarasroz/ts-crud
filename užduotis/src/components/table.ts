d28533cbc59cacba7542f06a172ebb3557100894
Aivaras Roz <aivaras.roz@gmail.com>
2023-01-24 19:44:18 +0200

    init: class with properties created. 4.2

--- užduotis/src/components/table.ts
+++ užduotis/src/components/table.ts
@@ -3,3 +3,11 @@ type TableProps<Type> = {
   columns: Type,
   rowsData: Type[],
 };
+
+
+class Table<Type> {
+  public htmlElement: HTMLTableElement;
+  private props: TableProps<Type>;
+  private tbody: HTMLTableSectionElement;
+  private thead: HTMLTableSectionElement;
+}
