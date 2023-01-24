c9867bcf6eaba3c1084860e3a504cb51d3407b33
Aivaras Roz <aivaras.roz@gmail.com>
2023-01-24 19:30:02 +0200

    init: implemented method with table display 3.3

--- užduotis/src/components/app.ts
+++ užduotis/src/components/app.ts
@@ -2,6 +2,8 @@ import CarsCollection from '../helpers/cars-collection';
 import cars from '../data/cars';
 import models from '../data/models';
 import brands from '../data/brands';
+import Table from './table';
+import stringifyProps from "../helpers/stringify-object";
 
 class App {
   private htmlElement: HTMLElement;
@@ -14,15 +16,26 @@ class App {
     
     this.carsCollection = new CarsCollection({models, brands, cars});
 
-    if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
+    if (foundElement === null) throw new Error(`Selector is not found.'${selector}'`);
 
     this.htmlElement = foundElement;
   }
 
   initialize = (): void => {
+    const tableCars = new Table({
+      title: 'Vehicles for Sale',
+      columns: {
+        id: 'id',
+        brand: 'Brand',
+        model: 'Model',
+        year: 'Year',
+        price: 'Price'
+      },
+      rowsData: this.carsCollection.all.map(stringifyProps),
+    })
     const container = document.createElement('div');
     container.className = 'container my-5';
-    container.innerHTML = 'Laukiu kol būsiu sukurtas';
+    container.appendChild(tableCars.htmlElement);
 
     this.htmlElement.append(container);
   };
