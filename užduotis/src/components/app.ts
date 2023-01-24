3d6859aa6ec890cc05376e270a062dbbd3eed84f
Aivaras Roz <aivaras.roz@gmail.com>
2023-01-24 19:11:08 +0200

    init: constructor selector, initial object 3.2 tsk

--- užduotis/src/components/app.ts
+++ užduotis/src/components/app.ts
@@ -1,4 +1,7 @@
-import type CarsCollection from '../helpers/cars-collection';
+import CarsCollection from '../helpers/cars-collection';
+import cars from '../data/cars';
+import models from '../data/models';
+import brands from '../data/brands';
 
 class App {
   private htmlElement: HTMLElement;
@@ -8,6 +11,8 @@ class App {
 
   constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
+    
+    this.carsCollection = new CarsCollection({models, brands, cars});
 
     if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
