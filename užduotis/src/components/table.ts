3a83272fa00a9d5c5ed0b204f06a9feba53f350b
Aivaras Roz <aivaras.roz@gmail.com>
2023-01-25 18:07:34 +0200

    init: crud 2 delete-task 2

--- užduotis/src/components/table.ts
+++ užduotis/src/components/table.ts
@@ -9,6 +9,7 @@ export type TableProps<Type> = {
   title: string,
   columns: Type,
   rowsData: Type[],
+  onDelete: (id: string) => void,
 };
 
 class Table<Type extends RowData> {
@@ -83,7 +84,7 @@ class Table<Type extends RowData> {
   };
 
   private addActionsCell = (rowHtmlElement: HTMLTableRowElement, id: string): void => {
-    const { } = this.props;
+    const { onDelete } = this.props;
 
     const buttonCell = document.createElement('td');
 
@@ -91,7 +92,7 @@ class Table<Type extends RowData> {
     deleteButton.type = 'button';
     deleteButton.innerHTML = 'Delete';
     deleteButton.className = 'btn btn-danger';
-    deleteButton.addEventListener('click', () => ());
+    deleteButton.addEventListener('click', () => onDelete(id));
     deleteButton.style.width = '80px';
 
     buttonCell.append(deleteButton);
