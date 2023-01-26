b19bf6bf7743702813d634694ce2ef5aff8a8b66
Aivaras Roz <aivaras.roz@gmail.com>
2023-01-26 19:42:27 +0200

    init: crud3 create-task 5.2(CarsCollection) Add()

--- užduotis/src/helpers/cars-collection.ts
+++ užduotis/src/helpers/cars-collection.ts
@@ -9,22 +9,28 @@ type CarsCollectionProps = {
   models: Model[],
 };
 
+export type CarProps = {
+  brandId: string,
+  modelId: string,
+  price: number,
+  year: number
+};
+
+const createId = (): string => String(Math.floor(Math.random() * 100000));
+
 class CarsCollection {
-  private props: CarsCollectionProps;
+  constructor(private props: CarsCollectionProps) { }
 
-  constructor(props: CarsCollectionProps) {
-    this.props = props;
-  }
-
-  private joinCar = ({ modelId, ...car}: Car) => {
+  private joinCar = ({ modelId, ...car }: Car) => {
     const { brands, models } = this.props;
+
     const carModel = models.find((model) => model.id === modelId);
     const carBrand = brands.find((brand) => brand.id === carModel?.brandId);
 
     return {
       ...car,
-      brand: (carBrand && carBrand.title) ?? 'unknown',
-      model: (carModel && carModel.title) ?? 'unknown',
+      brand: (carBrand && carBrand?.title) ?? 'unknown',
+      model: (carModel && carModel?.title) ?? 'unknown',
     };
   };
 
@@ -44,10 +50,29 @@ class CarsCollection {
       .map(this.joinCar);
 
     return brandCars;
-  }
-    public deleteCarById = (carId: string): void => {
-      this.props.cars = this.props.cars.filter((car) => car.id !== carId);
+  };
+
+  public deleteCarById = (carId: string): void => {
+    this.props.cars = this.props.cars.filter((car) => car.id !== carId);
+  };
+
+  public add = ({ modelId, brandId, ...carProps }: CarProps): void => {
+    const { models, brands, cars } = this.props;
+    const model = models.find((m) => m.id === modelId);
+    const brand = brands.find((b) => b.id === brandId);
+
+    if (!model || !brand) {
+      throw new Error('Netinkami duomenys sukurti automobilį');
+    }
+
+    const newCar: Car = {
+      id: createId(),
+      ...carProps,
+      modelId,
     };
-  }
+
+    cars.push(newCar);
+  };
+}
 
 export default CarsCollection;
